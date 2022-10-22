4daa22e27bfeb542290a949310d21fd1c7cf1238
Thomas James <thomasjames@MacBook-Pro-4.local>
2022-10-22 21:17:31 +0100

    EntryTable created.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
+import "./App.css";
+import { TextField } from "@mui/material";
 
-const HelloWorld = () => {
-  return <h1>Hello World!</h1>;
-}
+const BookRepo = () => {
+  return <h1>Chaucer Head Book Repository</h1>;
+};
+
+const EntryTable = () => {
+  const textFieldData = [
+    { id: "book-title", label: "Book Title", variant: "outlined" },
+    { id: "author", label: "Author", variant: "outlined" },
+    { id: "publisher", label: "Publisher", variant: "outlined" },
+  ];
+  // Not sure about this.
+  return textFieldData.map((book) => (
+    <TextField id={book.id} label={book.label} variant={book.variant} />
+  ));
+};
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld />
+      <BookRepo />
+      <EntryTable />
+      {/* <TextField id="book-title" label="Book Title" variant="outlined" />
+      <br />
+      <TextField id="author" label="Author" variant="outlined" />
+      <br />
+      <TextField id="publisher" label="Publisher" variant="outlined" /> */}
     </div>
   );
 }

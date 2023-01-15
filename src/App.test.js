17157661c20753448f2de00a09d0ee2671da546d
Thomas James <thomasjames@MacBook-Pro-4.local>
2023-01-15 20:36:57 +0000

    Add test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react"
 import App from "./App"
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("renders 'Book Title'", () => {
+  render(<App />)
+  const bookTitle = screen.getByLabelText(/Book Title/i)
+  expect(bookTitle).toBeInTheDocument()
+})
